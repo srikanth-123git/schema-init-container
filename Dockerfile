b6cd5b51bc002f07feea1d489cd399ead490b96f
Srikanth Palakuri <palakuri.srikanth23@gmail.com>
2024-07-29 00:29:32 +0530

    Update schema load

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,6 @@
 FROM          redhat/ubi9
-RUN           dnf install mysql git -y
+ADD           mongo.repo /etc/yum.repos.d/mongo.repo
+RUN           dnf install mysql git mongodb-org -y
 COPY          run.sh /
 ENTRYPOINT    ["bash", "/run.sh"]
 
--- run.sh
+++ run.sh
@@ -1,32 +1,12 @@
 source /data/secrets
-
-if [ -z "${DB_HOST}" ]; then
-  echo DB_HOST is missing
-  exit 1
-fi
-
-if [ -z "${RDS_USER}" ]; then
-  echo DB_USER is missing
-  exit 1
-fi
-
-if [ -z "${RDS_PASS}" ]; then
-  echo DB_HOST is missing
-  exit 1
-fi
-
-if [ -z "${COMPONENT}" ]; then
-  echo COMPONENT is missing
-  exit 1
-fi
-
-if [ -z "${PROJECT_NAME}" ]; then
-  echo PROJECT_NAME is missing
-  exit 1
-fi
-
 git clone https://github.com/srikanth-123git/${PROJECT_NAME}-${COMPONENT}
 cd ${PROJECT_NAME}-$COMPONENT
 
-mysql -h${DB_HOST} -u${RDS_USER} -p${RDS_PASS} <schema/${COMPONENT}.sql
+if [ "${db_type}" == 'mongo' ]; then
+  curl -L -O https://truststore.pki.rds.amazonaws.com/global/global-bundle.pem
+  mongo --tls --host $DB_HOST:27017 --tlsCAFile global-bundle.pem --username ${DB_USER} --password $DB_PASS <schema/${COMPONENT}.js
+fi
 
+if [ "${db_type}" == 'mysql' ]; then
+  mysql -h${DB_HOST} -u${RDS_USER} -p${RDS_PASS} <schema/${COMPONENT}.sql
+fi
